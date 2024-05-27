20ecaf641e86f90246fa01f97f2e56ef3c2826e4
Sonja Lecic <slecic8@gmail.com>
2024-05-27 12:32:14 +0200

    Update cheat.sh

--- cheat.sh
+++ cheat.sh
@@ -1,2 +1,5 @@
 # convert txt file into a tab delimited format
 awk '{ for(i=1;i<=NF;i++){if(i==NF){printf("%s\n",$NF);}else {printf("%s\t",$i)}}}' inputfile
+
+# exclude lines conditional on the values in columns
+awk 'NR==1 || !($2=="op2" && $3<5 && $3>1)' test.file
